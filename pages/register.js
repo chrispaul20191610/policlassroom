24a9653cd9aa9a68ae25938d721ad3bf6515a0f2
Ander <anderson.cordova@epn.edu.ec>
2021-08-28 10:21:59 -0500

    Add: Validaciones con yup en register

--- pages/register.js
+++ pages/register.js
@@ -2,13 +2,43 @@ import { useForm } from "react-hook-form";
 import { useState } from "react";
 import User from "./api/user";
 import Image from "next/image";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  name: yup.string().required("Este campo es obligatorio"),
+  last_name: yup.string().required("Este campo es obligatorio"),
+  description: yup.string().max(500).required("Este campo es obligatorio"),
+  role: yup.string().required("Este campo es obligatorio"),
+  email: yup
+    .string()
+    .email("Ingrese un correo válido")
+    .required("Este campo es obligatorio"),
+  password: yup
+    .string()
+    .min(6, "Ingrese al menos 6 caracteres")
+    .required("Este campo es obligatorio"),
+  password_confirmation: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Las claves no coinciden")
+    .required("Este campo es obligatorio"),
+  avatar: yup.string().required("Este campo es obligatorio"),
+});
 
 const register = () => {
-  const { register, handleSubmit, watch } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const [userInfo, setUserInfo] = useState(null);
   const [result, setResult] = useState(null);
-  const [errors, setError] = useState([]);
+  const [errorsList, setErrorsList] = useState([]);
 
   const onSubmit = async (formData) => {
     setUserInfo(null);
@@ -22,6 +52,7 @@ const register = () => {
       const response = await User.register(formData);
       console.log("response", response);
       setUserInfo(response.data);
+      reset();
       setResult("Usuario registrado correctamente =D");
     } catch (e) {
       console.log("e", e.response);
@@ -31,12 +62,12 @@ const register = () => {
       if (response) {
         if (response.data.errors) {
           const errors = response.data.errors;
-          const errorList = Object.values(errors);
+          const NewErrorList = Object.values(errors);
           /* const errorList = [];
           for (let field in errors) {
             errorList.push(errors[field]);
           } */
-          setError(errorList);
+          setErrorsList(NewErrorList);
         }
       }
     }
@@ -50,54 +81,86 @@ const register = () => {
   };
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <div>
       <form
         onSubmit={handleSubmit(onSubmit)}
         style={style}
         encType="multipart/form-data"
       >
-        <input
-          {...register("name", { required: true })}
-          type="name"
-          placeholder="Nombre"
-        />
-        <input
-          {...register("last_name", { required: true })}
-          type="last_name"
-          placeholder="Apellido"
-        />
-        <textarea
-          {...register("description", { required: true })}
-          placeholder="Descripción"
-        />
+        <div>
+          <input
+            {...register("name", { required: true })}
+            type="name"
+            placeholder="Nombre"
+          />
+          <p>{errors.name?.message}</p>
+        </div>
+
+        <div>
+          <input
+            {...register("last_name", { required: true })}
+            type="last_name"
+            placeholder="Apellido"
+          />
+          <p>{errors.last_name?.message}</p>
+        </div>
+
+        <div>
+          <textarea
+            {...register("description", { required: true })}
+            placeholder="Descripción"
+          />
+          <p>{errors.description?.message}</p>
+        </div>
+
         <div>
           <select {...register("role")}>
             <option value="ROLE_STUDENT">ESTUDIANTE</option>
             <option value="ROLE-TEACHER">PROFESOR</option>
           </select>
+          <p>{errors.role?.message}</p>
         </div>
-        <input
-          {...register("email", { required: true })}
-          type="email"
-          placeholder="Correo electrónico"
-        />
-        <input
-          {...register("password", { required: true })}
-          type="password"
-          placeholder="Contraseña"
-        />
-        <input
-          {...register("password_confirmation", { required: true })}
-          type="password"
-          placeholder="Confirme su contraseña"
-        />
 
-        <input
-          {...register("avatar", { required: true })}
-          type="file"
-          name="imagen"
-        />
+        <div>
+          <input
+            {...register("email", { required: true })}
+            type="email"
+            placeholder="Correo electrónico"
+          />
+          <p>{errors.email?.message}</p>
+        </div>
+
+        <div>
+          <input
+            {...register("password", { required: true })}
+            type="password"
+            placeholder="Contraseña"
+          />
+          <p>{errors.password?.message}</p>
+        </div>
+
+        <div>
+          <input
+            {...register("password_confirmation", { required: true })}
+            type="password"
+            placeholder="Confirme su contraseña"
+          />
+          <p>{errors.password_confirmation?.message}</p>
+        </div>
+
+        <div>
+          <input
+            {...register("avatar", { required: true })}
+            type="avatar"
+            placeholder="Avatar"
+          />
+          {/* <input
+            {...register("avatar", { required: true })}
+            type="file"
+            name="imagen"
+          /> */}
+          <p>{errors.avatar?.message}</p>
+        </div>
 
         <p>{result}</p>
         {userInfo && (
@@ -109,10 +172,11 @@ const register = () => {
             <div>Token: {userInfo.token}</div>
           </div>
         )}
-        {errors.length > 0 && (
+
+        {errorsList.length > 0 && (
           <ul>
-            {errors.map((error) => (
-              <li key={error}>{error}</li>
+            {errorsList.map((errorList) => (
+              <li key={errorList}>{errorList}</li>
             ))}
           </ul>
         )}
