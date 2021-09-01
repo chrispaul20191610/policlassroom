789b5d4ef315b62074b7c06741dd10b0a8d5c984
Christian_Llumiquinga <christian.llumiquinga@epn.edu.ec>
2021-09-01 00:50:02 -0500

    #3-dependencias

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,54 @@
-import '../styles/globals.css'
+/*import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+/*function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+/*}
+
+/*export default MyApp*/
+
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "@/styles/theme";
+import { AuthProvider } from "../contexts/auth";
+import Logout from "@/components/Logout";
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Policlassroom</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <AuthProvider>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+
+          <Logout />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </AuthProvider>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
